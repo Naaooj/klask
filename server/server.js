6d3a1bbb0b768c762ef3cee0ff2b0403705f4665
Johann BERNEZ <jbernez@pictet-technologies.com>
2020-03-25 21:07:17 +0100

    Adding socket

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
 
 const clientPath = `${__dirname}/../client`;
 
-app.use(express.static(clientPath))
+app.use(express.static(clientPath));
 
 const server = http.createServer(app);
+
+const io = socketio(server);
+
+// Listen to connection on the socket
+io.on('connection', (clientSock) => {
+    console.log('Client (re)connected', clientSock);
+    clientSock.emit('message', 'Connected to Klask messaging');
+
+    // Listen on client message
+    clientSock.on('message', (text) => {
+
+        // Broadcast message to all connected clients
+        io.emit('message', text);
+    });
+});
 
 server.on('error', (err) => {
     console.log('Server error: ', err);
